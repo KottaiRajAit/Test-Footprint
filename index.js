d8b1aad6ccefeb4d7060cbdbafe005e5dbda46ca
KottaiRajAit <119566130+KottaiRajAit@users.noreply.github.com>
2023-08-14 22:04:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,28 +11,30 @@ app.use(cors());
 const RECAPTCHA_SECRET_KEY = '6LfDsqYnAAAAAMxr6c0OKKQ3ABvXoAQaHkuQvijM'; // Replace with your reCAPTCHA secret key
 
 app.post('/api/postData', async (req, res) => {
-  const data = req.body;
-  const captchaToken = data.captchaToken;
-  console.log('Received data:', data);
-  try {
-    // Verify reCAPTCHA token
-    const response = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${captchaToken}`
-    );
-        console.log('response', response);
-    if (response.data.success) {
-      console.log('reCAPTCHA verification successful');
-      console.log('Received data:', data);
-      // Process the data and send a response if needed
-      res.json({ message: 'Data received and reCAPTCHA verified successfully' });
-    } else {
-      console.log('reCAPTCHA verification failed');
-      res.status(400).json({ error: 'reCAPTCHA verification failed' });
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
+
+  console.log('sample')
+  // const data = req.body;
+  // const captchaToken = data.captchaToken;
+  // console.log('Received data:', data);
+  // try {
+  //   // Verify reCAPTCHA token
+  //   const response = await axios.post(
+  //     `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${captchaToken}`
+  //   );
+  //       console.log('response', response);
+  //   if (response.data.success) {
+  //     console.log('reCAPTCHA verification successful');
+  //     console.log('Received data:', data);
+  //     // Process the data and send a response if needed
+  //     res.json({ message: 'Data received and reCAPTCHA verified successfully' });
+  //   } else {
+  //     console.log('reCAPTCHA verification failed');
+  //     res.status(400).json({ error: 'reCAPTCHA verification failed' });
+  //   }
+  // } catch (error) {
+  //   console.error('Error:', error);
+  //   res.status(500).json({ error: 'An error occurred' });
+  // }
 });
 
 app.listen(port, () => {
